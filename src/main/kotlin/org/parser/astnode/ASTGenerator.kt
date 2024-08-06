2cc778966a98847d980dcfeef2b8b7852b1cdf8f
IgnacioChevallier <ichevallierboutell@mail.austral.edu.ar>
2024-08-06 11:45:53 -0300

    Six Commit ~ ASTGenerator.kt

--- src/main/kotlin/org/parser/astnode/ASTGenerator.kt
+++ src/main/kotlin/org/parser/astnode/ASTGenerator.kt
@@ -1,9 +1,8 @@
 package org.parser.astnode
 
-import org.Location
 import org.lexer.Token
 
-class ASTGenerator(private val formulaMap: List<Pair<String, ArrayList<Token>>>) {
+class ASTGenerator(private val formulaMap: List<Pair<String, List<String>>>) {
 
     public fun generate(buffer : ArrayList<Token>): ASTNode {
         // Generate the AST from a list of ASTNodes
@@ -18,21 +17,21 @@ class FormulaFactory {
         // Create a default lexicon with some common tokens. It's the basic lexicon.
         return ASTGenerator(
             listOf(
-                "AssignmentNode" to arrayListOf(
-                    Token("IdentifierNode", "IdentifierNode", Location(0, 0)),
-                    Token("AssignationToken", "AssignationToken", Location(0, 0)),
-                    Token("ExpressionNode", "ExpressionNode", Location(0, 0))
+                "AssignmentNode" to listOf(
+                    "IdentifierNode",
+                    "AssignationToken",
+                    "ExpressionNode",
                 ),
-                "DeclarationNode" to arrayListOf(
-                    Token("DeclarationToken", "DeclarationToken", Location(0, 0)),
-                    Token("IdentifierNode", "IdentifierNode", Location(0, 0)),
-                    Token("ColonToken", "ColonToken", Location(0, 0)),
-                    Token("TypeToken", "TypeToken", Location(0, 0)),
+                "DeclarationNode" to listOf(
+                    "DeclarationToken",
+                    "IdentifierNode",
+                    "ColonToken",
+                    "TypeToken",
                 ),
-                "ExpressionNode" to arrayListOf(
-                    Token("ExpressionNode", "ExpressionNode", Location(0, 0)),
-                    Token("OperationToken", "OperationToken", Location(0, 0)),
-                    Token("ExpressionNode", "ExpressionNode", Location(0, 0)),
+                "ExpressionNode" to listOf(
+                    "ExpressionNode",
+                    "OperationToken",
+                    "ExpressionNode",
                 )
             )
         )
