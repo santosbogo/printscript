085d89a0684558c78488711ccbdf236e36f9ddda
IgnacioChevallier <ichevallierboutell@mail.austral.edu.ar>
2024-08-06 18:08:05 -0300

    Eighth Commit ~ ASTGenerator.kt continue

--- src/main/kotlin/org/parser/astnode/statementnode/PrintStatementNode.kt
+++ src/main/kotlin/org/parser/astnode/statementnode/PrintStatementNode.kt
@@ -1,8 +1,12 @@
 package org.parser.astnode.statementnode
 
 import org.Location
+import org.lexer.Token
+import org.parser.astnode.ASTNode
 import org.parser.astnode.astnodevisitor.ASTNodeVisitor
 import org.parser.astnode.expressionnode.ExpressionNode
+import org.parser.astnode.expressionnode.Literal
+import org.parser.astnode.expressionnode.LiteralValue
 
 class PrintStatementNode(
     override val type: String,
@@ -12,4 +16,30 @@ class PrintStatementNode(
     override fun accept(visitor: ASTNodeVisitor) {
         TODO("Not yet implemented")
     }
+
+    override fun generate(tokens: List<Token>): ASTNode {
+        return PrintStatementNode(
+            "PrintStatementNode",
+            tokens[0].location,
+            Literal(
+                type = "String",
+                location = tokens[2].location,
+                value = LiteralValue.StringValue(tokens[2].value)
+            )
+        )
+    }
+
+    override val formula: List<String> =  listOf(
+        "PrintToken",
+        "OpenParenthesisToken",
+        "ExpressionToken", // Aca podría ir cualquier cosa, pero no se me ocurre que poner
+        "CloseParenthesisToken",
+        "SemicolonToken",
+    )
+
+    constructor() : this(
+        type = "PrintStatementNode",
+        location = Location(0, 0),
+        expression = Literal("String", Location(0, 0), LiteralValue.StringValue(""))
+    )
 }
---
+++ src/main/kotlin/org/parser/astnode/statementnode/VariableDeclarationNode.kt
@@ -0,0 +1,61 @@
+package org.parser.astnode.statementnode
+
+import org.Location
+import org.lexer.Token
+import org.parser.astnode.ASTNode
+import org.parser.astnode.astnodevisitor.ASTNodeVisitor
+import org.parser.astnode.expressionnode.ExpressionNode
+import org.parser.astnode.expressionnode.Literal
+import org.parser.astnode.expressionnode.LiteralValue
+
+class VariableDeclarationNode(
+    override val type: String,
+    override val location: Location,
+    override val expression: ExpressionNode,
+    private val identifier: String,
+    private val init: ExpressionNode,
+    private val kind: String
+) : StatementNode {
+    override fun accept(visitor: ASTNodeVisitor) {
+        TODO("Not yet implemented")
+    }
+
+    override fun generate(tokens: List<Token>): ASTNode {
+        TODO("Aca habría que pensar la estructura del buffer y asignar cada cosa a su lugar")
+        return VariableDeclarationNode(
+            "VariableDeclarationNode",
+            tokens[0].location,
+            Literal(
+                type = "String",
+                location = tokens[5].location,
+                value = LiteralValue.StringValue(tokens[5].value)
+            ),
+            tokens[1].value,
+            Literal(
+                type = "String",
+                location = tokens[5].location,
+                value = LiteralValue.StringValue(tokens[5].value)
+            ),
+            tokens[3].value
+        )
+    }
+
+    override val formula: List<String> = listOf(
+        "DeclarationToken",
+        "IdentifierToken",
+        "ColonToken",
+        "NumberTypeToken",
+        "AssignationToken",
+        "ExpressionToken",
+        "SemicolonToken",
+    )
+
+    constructor() : this(
+        type = "VariableDeclarationNode",
+        location = Location(0, 0),
+        expression = Literal("String", Location(0, 0), LiteralValue.StringValue("")),
+        identifier = "",
+        init = Literal("String", Location(0, 0), LiteralValue.StringValue("")),
+        kind = ""
+    )
+}
