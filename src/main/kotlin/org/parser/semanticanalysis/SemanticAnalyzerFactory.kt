01def091e0444fc9bb85ff979f8012c9baaafc21
matias <mchialva@mail.austral.edu.ar>
2024-08-08 18:15:31 -0300

    sem5f

--- src/main/kotlin/org/parser/semanticanalysis/SemanticAnalyzerFactory.kt
+++ src/main/kotlin/org/parser/semanticanalysis/SemanticAnalyzerFactory.kt
@@ -1,13 +1,12 @@
 package org.parser.semanticanalysis
 
+import org.parser.semanticanalysis.semanticchecks.AssignmentTypeCheck
+import org.parser.semanticanalysis.semanticchecks.SemanticCheck
 import org.parser.semanticanalysis.semanticchecks.UndeclaredVariableCheck
 import org.parser.semanticanalysis.semanticchecks.VariableDeclarationCheck
 import org.parser.semanticanalysis.semanticchecks.VariableDeclarationTypeCheck
-import org.parser.semanticanalysis.semanticchecks.AssignmentTypeCheck
-import org.parser.semanticanalysis.semanticchecks.SemanticCheck
 import org.parser.semanticanalysis.semanticmodifier.SemanticMapModifier
 import org.parser.semanticanalysis.semanticmodifier.VariableDeclarationModifier
-
 class SemanticAnalyzerFactory {
     fun createDefaultSemanticAnalyzer(): SemanticAnalyzer {
         val modifiers: List<SemanticMapModifier> = listOf(
