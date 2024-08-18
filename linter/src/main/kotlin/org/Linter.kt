5a7a4455b3281a4052e9a4cf6e115001c7c451ab
matias <mchialva@mail.austral.edu.ar>
2024-08-18 17:13:19 -0300

    Linter factory, linter config finished. next phase -> testing.

--- linter/src/main/kotlin/org/Linter.kt
+++ linter/src/main/kotlin/org/Linter.kt
@@ -1,8 +1,14 @@
 package org
 
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+import org.checkvisitors.NamingFormatCheckVisitor
+import org.checkvisitors.PrintUseCheckVisitor
+import org.checkvisitors.UnusedVariableCheckVisitor
 import org.common.astnode.ProgramNode
 import org.common.astnode.astnodevisitor.ASTNodeVisitor
 import org.common.astnode.astnodevisitor.types.VisitorResult
+import org.common.expressionfactory.PatternFactory
 
 class Linter(private val checkVisitors: List<ASTNodeVisitor>) {
     private val warnings = mutableListOf<String>()
@@ -16,3 +22,26 @@ class Linter(private val checkVisitors: List<ASTNodeVisitor>) {
 
 }
 
+class LinterFactory(){
+    fun createLinter(jsonFile: String): Linter {
+        val mapper = jacksonObjectMapper()
+        val config: LinterConfig = mapper.readValue(jsonFile)
+
+        val checkVisitors = config.enabledChecks.mapNotNull { checkName ->
+            when (checkName) {
+                "UnusedVariableCheck" -> UnusedVariableCheckVisitor()
+                "NamingFormatCheck" -> NamingFormatCheckVisitor(config.namingPatternName, PatternFactory.getNamingFormatPattern(config.namingPatternName))
+                "PrintUseCheck" -> PrintUseCheckVisitor(config.printlnCheckEnabled)
+                else -> null
+            }
+        }
+
+        return Linter(checkVisitors)
+    }
+}
+
+data class LinterConfig(
+    val enabledChecks: List<String>,
+    val namingPatternName: String = "",
+    val printlnCheckEnabled: Boolean = false
+)
--- shared/src/main/kotlin/org/common/expressionfactory/PatternFactory.kt
+++ shared/src/main/kotlin/org/common/expressionfactory/PatternFactory.kt
@@ -7,9 +7,17 @@ class PatternFactory {
             return """^(IdentifierToken|StringToken|NumberToken)(\s*(PlusToken|MinusToken|MultiplyToken|DivisionToken)\s*(IdentifierToken|StringToken|NumberToken))*$"""
         }
 
-        fun getCamelcasePattern(): String {
-            //starts with a lowercase letter, followed by any number of letters or numbers, and then an uppercase letter, followed by any number of letters or numbers.
-            return """^[a-z][a-zA-Z0-9]*[A-Z][a-zA-Z0-9]*$"""
+        fun getNamingFormatPattern(namingPatternName: String): String {
+            if (namingPatternName == "camelcase") {
+                //starts with a lowercase letter, followed by any number of letters or numbers, and then an uppercase letter, followed by any number of letters or numbers.
+                return """^[a-z][a-zA-Z0-9]*[A-Z][a-zA-Z0-9]*$"""
+            } else if (namingPatternName == "snakecase") {
+                //starts with a lowercase letter, followed by any number of letters or numbers, and then an underscore, followed by any number of letters or numbers.
+                return """^[a-z][a-zA-Z0-9]*_[a-zA-Z0-9]*$"""
+            } else {
+                throw IllegalArgumentException("Invalid naming pattern name")
+            }
         }
+
     }
 }
