c7827f7337dafb39b4ca288a926fa74ca5a69df7
TomasValleDuran <tvalleduran@mail.austral.edu.ar>
2024-08-31 16:44:00 -0300

    refactored InterpreterVisitor

--- interpreter/src/main/kotlin/org/InterpreterVisitor.kt
+++ interpreter/src/main/kotlin/org/InterpreterVisitor.kt
@@ -88,67 +88,8 @@ class InterpreterVisitor : ASTNodeVisitor {
         val leftValue = leftResult.value
         val rightValue = rightResult.value
 
-        val resultLiteralValue: LiteralValue = when (node.operator) {
-            "+" -> {
-                when {
-                    (leftValue is LiteralValue.StringValue) || (rightValue is LiteralValue.StringValue) ->
-                        LiteralValue.StringValue(leftValue.toString() + rightValue.toString())
-
-                    leftValue is LiteralValue.NumberValue && rightValue is LiteralValue.NumberValue ->
-                        LiteralValue.NumberValue(
-                            handleNumberCase(leftValue.value.toDouble(), rightValue.value.toDouble()) { a, b -> a + b }
-                        )
-
-                    else -> throw UnsupportedOperationException("Unsupported types for +")
-                }
-            }
-
-            "-" -> {
-                when {
-                    leftValue is LiteralValue.NumberValue && rightValue is LiteralValue.NumberValue ->
-                        LiteralValue.NumberValue(
-                            handleNumberCase(leftValue.value.toDouble(), rightValue.value.toDouble()) { a, b -> a - b }
-                        )
-
-                    else -> throw UnsupportedOperationException("Unsupported types for -")
-                }
-            }
-
-            "*" -> {
-                when {
-                    leftValue is LiteralValue.NumberValue && rightValue is LiteralValue.NumberValue ->
-                        LiteralValue.NumberValue(
-                            handleNumberCase(leftValue.value.toDouble(), rightValue.value.toDouble()) { a, b -> a * b }
-                        )
-
-                    else -> throw UnsupportedOperationException("Unsupported types for *")
-                }
-            }
-
-            "/" -> {
-                when {
-                    rightValue is LiteralValue.NumberValue && rightValue.value.toDouble() == 0.0 ->
-                        throw ArithmeticException("Division by zero")
-
-                    leftValue is LiteralValue.NumberValue && rightValue is LiteralValue.NumberValue ->
-                        LiteralValue.NumberValue(
-                            handleNumberCase(leftValue.value.toDouble(), rightValue.value.toDouble()) { a, b -> a / b }
-                        )
-
-                    else -> throw UnsupportedOperationException("Unsupported types for /")
-                }
-            }
-
-            else -> {
-                throw UnsupportedOperationException("Unsupported operator: ${node.operator}")
-            }
-        }
+        val resultLiteralValue: LiteralValue = VisitorHelper().evaluateBinaryExpression(leftValue, rightValue, node.operator)
 
         return VisitorResult.LiteralValueResult(resultLiteralValue)
-    }
-
-    private fun handleNumberCase(left: Double, right: Double, operator: (Double, Double) -> Double): Number {
-        return if (left % 1 == 0.0 && right % 1 == 0.0) operator(left, right).toInt()
-        else operator(left, right)
     }
 }
