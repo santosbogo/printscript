15a1070480eaa8bf7e047694e075aa2353d044cb
IgnacioChevallier <ichevallierboutell@mail.austral.edu.ar>
2024-09-02 00:42:57 -0300

    First ideas for the IF implementation

--- parser/src/main/kotlin/org/Parser.kt
+++ parser/src/main/kotlin/org/Parser.kt
@@ -53,6 +53,7 @@ class Parser(
     // Simply get all the tokens concerning that structure and handle them
     private fun handleStructure(type: String, tokens: List<Token>, i: Int, buffer: ArrayList<Token>, statements: ArrayList<ASTNode>, result: ParserResult): Int {
         if (type == "IfToken") {
+            // FIXME(Por ahora modularize con utils, pero se podría hacer una interfaz Structure para for, while y IF y que tengan un método getTokens() )
             buffer.addAll(Utils().getIfTokens(tokens, i))
         } // Here you could add other structures as while or for
         val b = handleStatement(buffer, statements, result)
@@ -80,52 +81,6 @@ class Parser(
     private fun checkIfStructureToken(string: String): Boolean {
         val structureTokens = listOf("IfToken", "ForToken", "WhileToken")
         return structureTokens.contains(string)
-    }
-}
-
-class Utils {
-    fun getIfTokens(tokens: List<Token>, index: Int): ArrayList<Token> {
-        var openBraces = 0
-        val buffer = ArrayList<Token>()
-        for (i in index until tokens.size + 1) {
-            val token = tokens[i + 1]
-            buffer.add(token)
-
-            // To handle nested if statements
-            if (token.type == "OpenBraceToken") {
-                openBraces++
-            } else if (token.type == "CloseBraceToken") {
-                openBraces--
-                if (openBraces == 0) { // Indicates it is the end of the if statement
-                    if (checkIfElse(tokens, i)) {
-                        getElseTokens(tokens, i, buffer)
-                    }
-                    return buffer
-                }
-            }
-        }
-
-        return buffer
-    }
-
-    fun getElseTokens(tokens: List<Token>, i: Int, buffer: ArrayList<Token>) {
-        for (token in tokens.subList(i, tokens.size)) {
-            buffer.add(token)
-            if (token.type == "CloseBraceToken") {
-                return
-            }
-        }
-    }
-
-    private fun checkIfElse(tokens: List<Token>, i: Int): Boolean {
-        if (tokens.size <= i + 1) {
-            return false
-        } // To avoid out of bounds exception
-        return tokens[i + 1].equals("ElseToken") // Check if the next token is an else token
-    }
-
-    fun getFormula(buffer: List<Token>): String {
-        return buffer.joinToString(" ") { it.type }
     }
 }
 
