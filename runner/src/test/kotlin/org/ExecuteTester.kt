d3a08931e0a82a115c265fabcb257474e83ad617
TomasValleDuran <tvalleduran@mail.austral.edu.ar>
2024-09-05 15:38:54 -0300

    version 1.1.4-SNAPSHOT

--- runner/src/test/kotlin/org/ExecuteTester.kt
+++ runner/src/test/kotlin/org/ExecuteTester.kt
@@ -7,7 +7,7 @@ class ExecuteTester {
     @Test
     fun test() {
         val runner = Runner("1.1")
-        val content = File("/home/tomasvalle/projects/PrintScript/runner/src/test/resources/examples-v11/execute/invalid-argument-in-if.txt").readText()
+        val content = File("src/test/resources/examples-v11/execute/invalid-argument-in-if.txt").readText()
         val result = runner.execute(content)
         assert(true == true)
     }
