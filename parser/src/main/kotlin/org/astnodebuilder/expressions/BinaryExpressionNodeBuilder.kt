92ffe38ba21468bff704df1a21eb46943a8096de
TomasValleDuran <tvalleduran@mail.austral.edu.ar>
2024-09-06 23:11:13 -0300

    version 1.1.10: Changed BinaryExpressionNodeBuilder to work correctly for many operators and operands

--- parser/src/main/kotlin/org/astnodebuilder/expressions/BinaryExpressionNodeBuilder.kt
+++ parser/src/main/kotlin/org/astnodebuilder/expressions/BinaryExpressionNodeBuilder.kt
@@ -3,115 +3,97 @@ package org.astnodebuilder.expressions
 import org.Parser
 import org.Token
 import org.astnode.ASTNode
-import org.astnode.expressionnode.ExpressionNode
 import org.astnode.expressionnode.BinaryExpressionNode
+import org.astnode.expressionnode.ExpressionNode
 import org.astnode.expressionnode.IdentifierNode
 import org.astnode.expressionnode.LiteralNode
 import org.astnode.expressionnode.LiteralValue
+
 import org.astnodebuilder.ASTNodeBuilder
 import org.astnodebuilder.IdentifierNodeBuilder
 import org.expressionfactory.PatternFactory
+import java.util.LinkedList
 
 class BinaryExpressionNodeBuilder : ASTNodeBuilder {
     override val formula = PatternFactory.getBinaryExpressionPattern()
 
     override fun generate(tokens: List<Token>, parser: Parser): ASTNode {
         if (tokens.isEmpty()) throw IllegalArgumentException("Empty token list")
-
-        return parseAdditiveExpression(tokens, parser)
+        val postfixNotation = getPostfixNotation(tokens)
+        val expression = evaluatePostfixNotation(postfixNotation, parser)
+        return expression
     }
 
     override fun checkFormula(tokensString: String): Boolean {
         return Regex(formula).matches(tokensString)
     }
 
-    private fun parseAdditiveExpression(tokens: List<Token>, parser: Parser): ExpressionNode {
-        var left = parseMultiplicativeExpression(
-            tokens.subList(
-                0,
-                findOperatorIndex(tokens, setOf("PlusToken", "MinusToken"))
-            ),
-            parser
-        )
-
-        for (i in tokens.indices) {
-            if (tokens[i].type == "PlusToken" || tokens[i].type == "MinusToken") {
-                val operatorToken = tokens[i]
-                val rightTokens = tokens.subList(i + 1, tokens.size)
-                val right = parseMultiplicativeExpression(rightTokens, parser)
-
-                left = BinaryExpressionNode(
-                    type = "BinaryExpression",
-                    location = operatorToken.location,
-                    left = left,
-                    right = right,
-                    operator = operatorToken.value
-                )
+    private fun getPostfixNotation(tokens: List<Token>): List<Token> {
+        val postfixNotation = mutableListOf<Token>()
+        val stack = LinkedList<Token>()
+        val operators = setOf("PlusToken", "MinusToken", "MultiplyToken", "DivisionToken")
+        for (token in tokens) {
+            if (token.type == "OpenParenthesisToken") {
+                stack.addLast(token)
+            } else if (token.type == "CloseParenthesisToken") {
+                while (stack.isNotEmpty() && stack.last().type != "OpenParenthesisToken") {
+                    postfixNotation.add(stack.removeLast())
+                }
+                stack.removeLast()
+            } else if (operators.contains(token.type)) {
+                while (stack.isNotEmpty() && operators.contains(stack.peekLast().type)) {
+                    postfixNotation.add(stack.removeLast())
+                }
+                stack.addLast(token)
+            } else {
+                postfixNotation.add(token)
             }
         }
-
-        return left
+        while (stack.isNotEmpty()) {
+            postfixNotation.add(stack.removeLast())
+        }
+        return postfixNotation
     }
 
-    private fun parseMultiplicativeExpression(tokens: List<Token>, parser: Parser): ExpressionNode {
-        var left = parsePrimaryExpression(
-            tokens.subList(
-                0,
-                findOperatorIndex(tokens, setOf("MultiplyToken", "DivisionToken"))
-            ),
-            parser
-        )
-
-        for (i in tokens.indices) {
-            if (tokens[i].type == "MultiplyToken" || tokens[i].type == "DivisionToken") {
-                val operatorToken = tokens[i]
-                val rightTokens = tokens.subList(i + 1, tokens.size)
-                val right = parsePrimaryExpression(rightTokens, parser)
-
-                left = BinaryExpressionNode(
-                    type = "BinaryExpression",
-                    location = operatorToken.location,
-                    left = left,
-                    right = right,
-                    operator = operatorToken.value
+    private fun evaluatePostfixNotation(postfixNotation: List<Token>, parser: Parser): ASTNode {
+        val stack = LinkedList<ASTNode>()
+        val operators = setOf("PlusToken", "MinusToken", "MultiplyToken", "DivisionToken")
+        for (token in postfixNotation) {
+            if (operators.contains(token.type)) {
+                val right = stack.removeLast()
+                val left = stack.removeLast()
+                stack.addLast(
+                    BinaryExpressionNode(
+                        type = "BinaryExpression",
+                        location = token.location,
+                        left = left as ExpressionNode,
+                        right = right as ExpressionNode,
+                        operator = token.value
+                    )
+                )
+            } else {
+                stack.addLast(
+                    when (token.type) {
+                        "IdentifierToken" -> IdentifierNodeBuilder().generate(listOf(token), parser) as IdentifierNode
+                        "StringToken" -> LiteralNode(
+                            type = "Literal",
+                            location = token.location,
+                            value = LiteralValue.StringValue(token.value)
+                        )
+                        "NumberToken" -> LiteralNode(
+                            type = "Literal",
+                            location = token.location,
+                            value = LiteralValue.NumberValue(checkIfInteger(token.value.toDouble()))
+                        )
+                        else -> throw IllegalArgumentException("Invalid token type")
+                    }
                 )
             }
         }
-
-        return left
-    }
-
-    private fun parsePrimaryExpression(tokens: List<Token>, parser: Parser): ExpressionNode {
-        if (tokens.size == 1) {
-            return when (tokens[0].type) {
-                "NumberToken" -> LiteralNode(
-                    type = "Literal",
-                    location = tokens[0].location,
-                    value = LiteralValue.NumberValue(checkIfInteger(tokens[0].value.toDouble()))
-                )
-                "StringToken" -> LiteralNode(
-                    type = "Literal",
-                    location = tokens[0].location,
-                    value = LiteralValue.StringValue(tokens[0].value)
-                )
-                "IdentifierToken" -> IdentifierNodeBuilder().generate(tokens, parser) as IdentifierNode
-                else -> throw IllegalArgumentException("Unexpected token type: ${tokens[0].type}")
-            }
-        } else {
-            return generate(tokens, parser) as ExpressionNode
-        }
+        return stack.removeLast()
     }
 
     private fun checkIfInteger(num: Double): Number {
         return if (num % 1 == 0.0) num.toInt() else num
-    }
-
-    private fun findOperatorIndex(tokens: List<Token>, operators: Set<String>): Int {
-        for (i in tokens.indices) {
-            if (tokens[i].type in operators) {
-                return i
-            }
-        }
-        return tokens.size // If no operator found, return the size of the list
     }
 }
--- runner/src/test/kotlin/org/ExecuteTester.kt
+++ runner/src/test/kotlin/org/ExecuteTester.kt
@@ -9,7 +9,7 @@ class ExecuteTester {
     @Test
     fun test() {
         val runner = Runner("1.1",)
-        val content = File("src/test/resources/examples-v11/execute/arithmetic-operations-decimal.txt").readText()
+        val content = File("src/test/resources/examples-v11/execute/read-input.txt").readText()
         val inputProvider = TestInputProvider(LinkedList(listOf("world")))
         val result = runner.execute(content, "1.1", inputProvider)
         assert(true == true)
